743ee1721732cb4ead8517bafcbc896f2960bb06
shreyaB7134 <shreyaburra18@gmail.com>
2025-07-31 21:34:42 +0530

    Fixed homepage table link for Day 75 - MyPaint

--- index.js
+++ index.js
@@ -157,7 +157,10 @@ function fillTable() {
           ["Day 58", "Budget Tracker", "public/Budget-Tracker/index.html"],
 
         ["Day 70", "Memory Game App", "./public/Memory Game App/index.html"],
-         ["Day 75", "MyPaint", "/public/day75-mypaint/index.html"],  
+  
+         ["Day 75", "MyPaint", "public/day75-mypaint/index.html"],
+
+  
 ];
 
 
